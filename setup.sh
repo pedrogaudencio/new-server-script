6dfd618a94bec4221f6491ce29eb4a9ba885ed90
roscas <sol.rosca@gmail.com>
2020-08-31 22:44:10 +0200

    initial commit

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ sudo apt update;
 sudo apt upgrade;
 sudo apt install zsh;
 chsh -s /bin/zsh $USER;
-mv .zshrc ~/
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mv .zshrc ~/
 
 git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions/"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting/"
